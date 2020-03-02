a264759f20677884298ee63b5254beeab493f481
nothingtosee <warrockph24@gmail.com>
2020-03-03 05:44:13 +0800

    Auto submit, button removed

--- dist/vue2-recaptcha-invisible.esm.js
+++ dist/vue2-recaptcha-invisible.esm.js
@@ -1,91 +1,81 @@
 (function(){ if(typeof document !== 'undefined'){ var head=document.head||document.getElementsByTagName('head')[0], style=document.createElement('style'), css=""; style.type='text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style); } })();
-
-
-
-
-
-
-
-
-
-
 /*
 global window document
 */
-var component = {render: function(){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('div',[_c('button',{class:_vm.dataBtnClass,attrs:{"disabled":_vm.dataBtnDisabled},on:{"click":_vm.submitData}},[_vm._t("default",[_vm._v("SUBMIT")])],2)])])},staticRenderFns: [],
-  props: {
-    dataSitekey: String,
-    dataCallback: Function,
-    dataValidate: Function,
-    dataBadge: String,
-    dataType: String,
-    dataErrorcallback: Function,
-    dataTabindex: String,
-    dataSize: String,
-    dataBtnClass: [String, Array, Object],
-    dataBtnDisabled: Boolean,
-    dataLanguage: String,
-  },
-  data: function data() {
-    return {
-      recaptchaId: 0,
-      vueRecaptchaInit: 0,
-    };
-  },
-  created: function created() {
-    var this$1 = this;
+var component = {
+	props: {
+		dataSitekey: String,
+		dataCallback: Function,
+		dataValidate: Function,
+		dataBadge: String,
+		dataType: String,
+		dataErrorcallback: Function,
+		dataTabindex: String,
+		dataSize: String,
+		dataLanguage: String,
+	},
+	data: function data() {
+		return {
+			recaptchaId: 0,
+			vueRecaptchaInit: 0,
+		};
+	},
+	created: function created() {
+		var this$1 = this;
 
-    if (typeof window === 'undefined') { return; }
-    window.vueRecaptchaInit = function () {
-      this$1.vueRecaptchaInit++;
-    };
-    var recaptchaScript = document.createElement('script');
-    var language = this.dataLanguage ? ("&hl=" + (this.dataLanguage)) : '';
-    recaptchaScript.setAttribute('src', ("https://www.google.com/recaptcha/api.js?onload=vueRecaptchaInit&render=explicit" + language));
-    recaptchaScript.setAttribute('async', '');
-    recaptchaScript.setAttribute('defer', '');
-    (document.body || document.head).appendChild(recaptchaScript);
-  },
-  watch: {
-    vueRecaptchaInit: function vueRecaptchaInit() {
-      try {
-        var options = {
-          sitekey: this.dataSitekey,
-        };
-        if (typeof this.dataBadge !== 'undefined') { options.badge = this.dataBadge; }
-        if (typeof this.dataType !== 'undefined') { options.type = this.dataType; }
-        if (typeof this.dataTabindex !== 'undefined') { options.tabindex = this.dataTabindex; }
-        if (typeof this.dataSize === 'undefined') {
-          options.size = 'invisible';
-          options.callback = this.getToken;
-        }
-        var recaptchaDiv = document.createElement('div');
-        recaptchaDiv.className = 'outside-badge';
-        this.$el.insertBefore(recaptchaDiv, this.$el.childNodes[0]);
-        this.recaptchaId = window.grecaptcha.render(recaptchaDiv, options);
-      } catch (e) {
-        window.console.error(e);
-      }
-    },
-  },
-  methods: {
-    submitData: function submitData(event) {
-      event.preventDefault();
-      if (typeof window === 'undefined') { return; }
-      if (this.dataValidate() === true || typeof this.dataValidate === 'undefined') {
-        if (typeof this.dataSize === 'undefined') {
-          window.grecaptcha.execute(this.recaptchaId);
-        } else {
-          this.getToken(window.grecaptcha.getResponse(this.recaptchaId));
-        }
-      }
-    },
-    getToken: function getToken(token) {
-      if (typeof window === 'undefined') { return; }
-      window.grecaptcha.reset(this.recaptchaId);
-      this.dataCallback(token);
-    },
-  },
+		if (typeof window === 'undefined') { return; }
+		window.vueRecaptchaInit = function () {
+			this$1.vueRecaptchaInit++;
+		};
+		var recaptchaScript = document.createElement('script');
+		var language = this.dataLanguage ? ("&hl=" + (this.dataLanguage)) : '';
+		recaptchaScript.setAttribute('src', ("https://www.google.com/recaptcha/api.js?onload=vueRecaptchaInit&render=explicit" + language));
+		recaptchaScript.setAttribute('async', '');
+		recaptchaScript.setAttribute('defer', '');
+		(document.body || document.head).appendChild(recaptchaScript);
+	},
+	watch: {
+		vueRecaptchaInit: function vueRecaptchaInit() {
+			try {
+				var options = {
+					sitekey: this.dataSitekey,
+				};
+				if (typeof this.dataBadge !== 'undefined') { options.badge = this.dataBadge; }
+				if (typeof this.dataType !== 'undefined') { options.type = this.dataType; }
+				if (typeof this.dataTabindex !== 'undefined') { options.tabindex = this.dataTabindex; }
+				if (typeof this.dataSize === 'undefined') {
+					options.size = 'invisible';
+					options.callback = this.getToken;
+				}
+				var recaptchaDiv = document.createElement('div');
+				recaptchaDiv.className = 'outside-badge';
+				this.$el.insertBefore(recaptchaDiv, this.$el.childNodes[0]);
+				this.recaptchaId = window.grecaptcha.render(recaptchaDiv, options);
+			} catch (e) {
+				window.console.error(e);
+			}
+		},
+	},
+	methods: {
+		submitData: function submitData() {
+			if (typeof window === 'undefined') { return; }
+			if (this.dataValidate() === true || typeof this.dataValidate === 'undefined') {
+				if (typeof this.dataSize === 'undefined') {
+					window.grecaptcha.execute(this.recaptchaId);
+				} else {
+					this.getToken(window.grecaptcha.getResponse(this.recaptchaId));
+				}
+			}
+		},
+		getToken: function getToken(token) {
+			if (typeof window === 'undefined') { return; }
+			window.grecaptcha.reset(this.recaptchaId);
+			this.dataCallback(token);
+		},
+	},
+	mounted: function mounted() {
+		this.submitData();
+	},
 };
 
 // Import vue component
--- src/reCaptchaComponent.vue
+++ src/reCaptchaComponent.vue
@@ -1,88 +1,78 @@
-<template>
-<div>
-  <div>
-    <button @click="submitData" :class="dataBtnClass" :disabled="dataBtnDisabled">
-      <slot>SUBMIT</slot>
-    </button>
-  </div>
-</div>
-</template>
-
 <script>
 /*
 global window document
 */
 export default {
-  props: {
-    dataSitekey: String,
-    dataCallback: Function,
-    dataValidate: Function,
-    dataBadge: String,
-    dataType: String,
-    dataErrorcallback: Function,
-    dataTabindex: String,
-    dataSize: String,
-    dataBtnClass: [String, Array, Object],
-    dataBtnDisabled: Boolean,
-    dataLanguage: String,
-  },
-  data() {
-    return {
-      recaptchaId: 0,
-      vueRecaptchaInit: 0,
-    };
-  },
-  created() {
-    if (typeof window === 'undefined') return;
-    window.vueRecaptchaInit = () => {
-      this.vueRecaptchaInit++;
-    };
-    const recaptchaScript = document.createElement('script');
-    const language = this.dataLanguage ? `&hl=${this.dataLanguage}` : '';
-    recaptchaScript.setAttribute('src', `https://www.google.com/recaptcha/api.js?onload=vueRecaptchaInit&render=explicit${language}`);
-    recaptchaScript.setAttribute('async', '');
-    recaptchaScript.setAttribute('defer', '');
-    (document.body || document.head).appendChild(recaptchaScript);
-  },
-  watch: {
-    vueRecaptchaInit() {
-      try {
-        const options = {
-          sitekey: this.dataSitekey,
-        };
-        if (typeof this.dataBadge !== 'undefined') options.badge = this.dataBadge;
-        if (typeof this.dataType !== 'undefined') options.type = this.dataType;
-        if (typeof this.dataTabindex !== 'undefined') options.tabindex = this.dataTabindex;
-        if (typeof this.dataSize === 'undefined') {
-          options.size = 'invisible';
-          options.callback = this.getToken;
-        }
-        const recaptchaDiv = document.createElement('div');
-        recaptchaDiv.className = 'outside-badge';
-        this.$el.insertBefore(recaptchaDiv, this.$el.childNodes[0]);
-        this.recaptchaId = window.grecaptcha.render(recaptchaDiv, options);
-      } catch (e) {
-        window.console.error(e);
-      }
-    },
-  },
-  methods: {
-    submitData(event) {
-      event.preventDefault();
-      if (typeof window === 'undefined') return;
-      if (this.dataValidate() === true || typeof this.dataValidate === 'undefined') {
-        if (typeof this.dataSize === 'undefined') {
-          window.grecaptcha.execute(this.recaptchaId);
-        } else {
-          this.getToken(window.grecaptcha.getResponse(this.recaptchaId));
-        }
-      }
-    },
-    getToken(token) {
-      if (typeof window === 'undefined') return;
-      window.grecaptcha.reset(this.recaptchaId);
-      this.dataCallback(token);
-    },
-  },
+	props: {
+		dataSitekey: String,
+		dataCallback: Function,
+		dataValidate: Function,
+		dataBadge: String,
+		dataType: String,
+		dataErrorcallback: Function,
+		dataTabindex: String,
+		dataSize: String,
+		dataLanguage: String,
+	},
+	data() {
+		return {
+			recaptchaId: 0,
+			vueRecaptchaInit: 0,
+		};
+	},
+	created() {
+		if (typeof window === 'undefined') return;
+		window.vueRecaptchaInit = () => {
+			this.vueRecaptchaInit++;
+		};
+		const recaptchaScript = document.createElement('script');
+		const language = this.dataLanguage ? `&hl=${this.dataLanguage}` : '';
+		recaptchaScript.setAttribute('src', `https://www.google.com/recaptcha/api.js?onload=vueRecaptchaInit&render=explicit${language}`);
+		recaptchaScript.setAttribute('async', '');
+		recaptchaScript.setAttribute('defer', '');
+		(document.body || document.head).appendChild(recaptchaScript);
+	},
+	watch: {
+		vueRecaptchaInit() {
+			try {
+				const options = {
+					sitekey: this.dataSitekey,
+				};
+				if (typeof this.dataBadge !== 'undefined') options.badge = this.dataBadge;
+				if (typeof this.dataType !== 'undefined') options.type = this.dataType;
+				if (typeof this.dataTabindex !== 'undefined') options.tabindex = this.dataTabindex;
+				if (typeof this.dataSize === 'undefined') {
+					options.size = 'invisible';
+					options.callback = this.getToken;
+				}
+				const recaptchaDiv = document.createElement('div');
+				recaptchaDiv.className = 'outside-badge';
+				this.$el.insertBefore(recaptchaDiv, this.$el.childNodes[0]);
+				this.recaptchaId = window.grecaptcha.render(recaptchaDiv, options);
+			} catch (e) {
+				window.console.error(e);
+			}
+		},
+	},
+	methods: {
+		submitData() {
+			if (typeof window === 'undefined') return;
+			if (this.dataValidate() === true || typeof this.dataValidate === 'undefined') {
+				if (typeof this.dataSize === 'undefined') {
+					window.grecaptcha.execute(this.recaptchaId);
+				} else {
+					this.getToken(window.grecaptcha.getResponse(this.recaptchaId));
+				}
+			}
+		},
+		getToken(token) {
+			if (typeof window === 'undefined') return;
+			window.grecaptcha.reset(this.recaptchaId);
+			this.dataCallback(token);
+		},
+	},
+	mounted() {
+		this.submitData();
+	},
 };
 </script>
